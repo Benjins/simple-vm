a7de041fd8bef9b4dea516e8bf30f9554a7f8527
Benji Smith <smith.benj@husky.neu.edu>
2015-01-14 13:16:55 -0500

    Better test.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using std::cout; using std::endl;
 
 int main(){
 
-	string code = "if(READ() > 3){PRINT(0);}PRINT(1);";
+	string code = "if(READ() > 3){if(READ() > 5){PRINT(0);}}PRINT(1);";
 	vector<string> tokens = NewTokenize(code);
 	
 	vector<unsigned char> byteCode = NewShuntingYard(tokens);
