a9789383ab34b314f94b2246186c8d526cddada1
Benji Smith <smith.benj@husky.neu.edu>
2015-10-03 23:58:17 -0400

    Coverall testing.

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,12 +1,18 @@
 #!bin/bash
+mv *.gcda src/
+mv *.gcno src/
 gcov -r src/*.cpp > coverage.txt
+ls src
 g++ scripts/coveralls.cpp -o  coveralls_cpp
 ARGS_ARRAY=("$COV_TOKEN")
 
 for f in `ls *.gcov`
 do
+if  [[ "$f" != /* ]] ;
+ then
   ARGS_ARRAY+=("$f");
   ARGS_ARRAY+=(`md5sum $f | awk '{ print $1 }'`); 
+ fi
 done
 
 ./coveralls_cpp ${ARGS_ARRAY[*]}
