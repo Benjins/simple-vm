e1a6f389766545af3b06974cdf3aae698d831ae9
Benji Smith <smith.benj@husky.neu.edu>
2015-10-03 23:47:36 -0400

    Coverall testing.

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,19 +1,14 @@
 #!bin/bash
-mv *.gcda src/
-mv *.gcno src/
 gcov -r src/*.cpp > coverage.txt
 g++ scripts/coveralls.cpp -o  coveralls_cpp
 ARGS_ARRAY=("$COV_TOKEN")
 
 for f in `ls *.gcov`
 do
-if  [[ "$f" != /* ]] ;
- then
   ARGS_ARRAY+=("$f");
   ARGS_ARRAY+=(`md5sum $f | awk '{ print $1 }'`); 
- fi
 done
 
 ./coveralls_cpp ${ARGS_ARRAY[*]}
-cat coverage.json
-#curl -H "Content-Type: application/json" --data @coverage.json https://coveralls.io/api/v1/jobs
+cat coveralls.json
+curl -H "Content-Type: application/json" --data @coveralls.json https://coveralls.io/api/v1/jobs
